b5b69110b499602c0e18b4922406d0e73d725e49
kym18 <cooiyoo@naver.com>
2023-10-05 13:21:54 +0900

    boy class 생성

--- boy_grass_object.py
+++ boy_grass_object.py
@@ -1,16 +1,41 @@
 from pico2d import *
+import random
 
 
 # Game object class here
-class Grass: #클래스이름은 대문자로 시작하는 명사
-    def __init__(self): #생성자함수
+class Grass:  # 클래스이름은 대문자로 시작하는 명사
+    def __init__(self):  # 생성자함수
         self.image = load_image('grass.png')
-
 
     def draw(self):
         self.image.draw(400, 30)
 
-    def update(self): pass
+    def update(self):
+        pass
+
+class Ball1:  # 클래스이름은 대문자로 시작하는 명사
+    def __init__(self):  # 생성자함수
+        self.image = load_image('ball21x21.png')
+        self.x, self.y = random.randint(0, 700), 599
+
+    def draw(self):
+        self.image.draw(self.x, self.y)
+
+    def update(self):
+        if self.y >= 30:
+            self.y -= 5
+
+
+
+class Ball2:  # 클래스이름은 대문자로 시작하는 명사
+    def __init__(self):  # 생성자함수
+        self.image = load_image('ball41x41.png')
+        self.x, self.y = random.randint(100, 700), 599
+    def draw(self):
+        self.image.draw(self.x, self.y)
+
+    def update(self):
+        self.y -= 5
 
 
 def handle_events():
@@ -26,19 +51,45 @@ def handle_events():
 def reset_world():
     global running
     global grass
+    global team1
+    global team2
+    global world1  # 현재 월드에 존재하는 모든객체 담은 리스트
+    global world2 # 현재 월드에 존재하는 모든객체 담은 리스트
 
     running = True
+    world1 = []
+    world2 = []
+
     grass = Grass()  # class를 이용해서 객체를 찍어냄
+    world1.append(grass)
+    world2.append(grass)
+
+    team1 = [Ball1() for i in range(10)]
+    team2 = [Ball2() for i in range(10)]
+    world1 += team1
+    world2 += team2
+
 
 def render_world():
     clear_canvas()
-    grass.draw() #self는 클래스 안 함수정의할때만. (호출할ㄷ떈 없음)
+    # grass.draw()  # self는 클래스 안 함수정의할때만. (호출할ㄷ떈 없음)
+    # for boy in team:
+    #     boy.draw()
+    for o in world1:
+        o.draw()
+    for o in world2:
+        o.draw()
     update_canvas()
 
 
 def update_world():
-    grass.update()
-    pass
+    # grass.update()
+    # for boy in team:
+    #     boy.update()
+    for o in world1:
+        o.update()
+    for o in world2:
+        o.update()
 
 
 open_canvas()
